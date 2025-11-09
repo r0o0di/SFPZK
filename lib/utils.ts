be29516f9cb0db3263c53150b347668442523cc6
r0o0di <rodikhello2000@gmail.com>
2025-11-09 20:00:36 +0100

    fixed minor bugs

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { auth } from '@/lib/firebase';
+import { User } from 'firebase/auth';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -17,7 +18,7 @@ const Utilities = {
    const adminList = ['rodikhello2000@gmail.com'];
     return adminList.includes(email || '')
   },
-  onAuthChange(callback: (user: any) => void) {
+  onAuthChange(callback: (user: User | null) => void) {
     return auth.onAuthStateChanged(callback)
   }
 
