f7599ddb39dd3de1cccf496fb200ee20ed4ffd92
r0o0di <rodikhello2000@gmail.com>
2026-06-27 19:11:35 +0200

    added meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { Toaster } from "@/components/ui/sonner"
-import { SpeedInsights } from '@vercel/speed-insights/next';
-
+import { Toaster } from "@/components/ui/sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,9 +16,59 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+export const viewport: Viewport = {
+  themeColor: "#7bf1a8",
+};
+
 export const metadata: Metadata = {
+  metadataBase: new URL("https://sfpzk.vercel.app"),
+
   title: "SFPZK",
-  description: "SFPZK - Şaxa Ewropa",
+
+  description: "SFPZK - Saziya Fêrkirin û Parastina Zimanê Kurdî",
+
+  keywords: [
+    "SFPZK",
+    "Saziya",
+    "Fêrkirin",
+    "Zimanê Kurdî",
+    "Kurdish Language",
+    "Learning",
+    "Education",
+    "Kurmanji",
+  ],
+
+  other: {
+    google: "notranslate",
+  },
+
+  openGraph: {
+    locale: "ku_IQ",
+    siteName: "SFPZK",
+    url: "https://sfpzk.vercel.app",
+    type: "website",
+    title: "SFPZK",
+    description: "SFPZK - Saziya Fêrkirin û Parastina Zimanê Kurdî",
+    images: [
+      {
+        url: "/sfpzk-logo.png",
+        width: 1200,
+        height: 630,
+        alt: "SFPZK",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "SFPZK",
+    description: "SFPZK - Saziya Fêrkirin û Parastina Zimanê Kurdî",
+    images: ["/sfpzk-logo.png"],
+  },
+
+  alternates: {
+    canonical: "https://sfpzk.vercel.app",
+  },
 };
 
 export default function RootLayout({
