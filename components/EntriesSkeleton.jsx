4fc41df0fb03bb65a5b006eeb68d2f441e24aa83
r0o0di <rodikhello2000@gmail.com>
2026-08-17 15:07:02 +0200

    minor entreisSkeleton change

--- components/EntriesSkeleton.jsx
+++ components/EntriesSkeleton.jsx
@@ -7,7 +7,7 @@ export default function EntriesSkeleton() {
     return (
         <div className="max-w-4xl mx-auto mb-8">
             <Card className="w-full bg-gray-800">
-                <CardHeader className="mt-2">
+                <CardHeader className="mt-3">
                     <Skeleton className="h-4 w-2/3 bg-gray-700" />
                     <Skeleton className="h-4 w-1/4 bg-gray-700" />
                 </CardHeader>
@@ -20,17 +20,23 @@ export default function EntriesSkeleton() {
                         <Skeleton className="h-4 w-1/2 bg-gray-700" />
                     </div>
 
-                    <div className="grid grid-cols-2 gap-2">
-                        <Skeleton className="aspect-video w-full bg-gray-700" />
-                        <Skeleton className="aspect-video w-full bg-gray-700" />
+                    {/* Top Row */}
+                    <div className="grid grid-cols-2 gap-1">
+                        {/* Top-Left: Rounded Top-Left corner only */}
+                        <Skeleton className="aspect-video w-full bg-gray-700 rounded-none rounded-tl-lg" />
+                        {/* Top-Right: Rounded Top-Right corner only */}
+                        <Skeleton className="aspect-video w-full bg-gray-700 rounded-none rounded-tr-lg" />
                     </div>
-                    <div className="grid grid-cols-2 gap-2 mt-2">
-                        <Skeleton className="aspect-video w-full bg-gray-700" />
-                        <Skeleton className="aspect-video w-full bg-gray-700" />
+
+                    {/* Bottom Row */}
+                    <div className="grid grid-cols-2 gap-1 mt-1">
+                        {/* Bottom-Left: Rounded Bottom-Left corner only */}
+                        <Skeleton className="aspect-video w-full bg-gray-700 rounded-none rounded-bl-lg" />
+                        {/* Bottom-Right: Rounded Bottom-Right corner only */}
+                        <Skeleton className="aspect-video w-full bg-gray-700 rounded-none rounded-br-lg" />
                     </div>
                 </CardContent>
             </Card>
-
         </div>
     )
 }
